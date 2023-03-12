b77941cabe9f901bf113fe398a99bedcd16a9a9a
Gemesil <ofekyvb@gmail.com>
2023-03-12 17:57:39 +0200

    layout changes in mainpage and searchpage, searchpage searchbar functionality, conversion to ListView

--- JobHawk/display/urls.py
+++ JobHawk/display/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import MainPage, SearchPage
+from .views import MainPageView, SearchPageView
 
 urlpatterns = [
-    path('', MainPage.as_view(), name="home"),
-    path('search', SearchPage.as_view(), name="search_results"),
+    path('', MainPageView.as_view(), name="home"),
+    path('search', SearchPageView.as_view(), name="search_results"),
 ]
--- JobHawk/display/views.py
+++ JobHawk/display/views.py
@@ -6,10 +6,16 @@ from .forms import SearchForm
 from .models import Job
 
 
-class MainPage(TemplateView):
+class MainPageView(TemplateView):
     template_name = 'display/main_page.html'
 
 
-class SearchPage(ListView):
+class SearchPageView(ListView):
     model = Job
     template_name = 'display/search_page.html'
+    context_object_name = 'results'
+    paginate_by = 10
+
+    def get_queryset(self):
+        query = self.request.GET.get('query')
+        return Job.objects.filter(title__icontains=query)
