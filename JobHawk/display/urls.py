18d9411cc52733329362b492637ccd06ec2aedb6
Gemesil <ofekyvb@gmail.com>
2023-03-12 18:07:50 +0200

    query on jobs db from searchbar implementation

--- JobHawk/display/urls.py
+++ JobHawk/display/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import MainPageView, SearchPageView
 
 urlpatterns = [
-    path('', MainPageView.as_view(), name="home"),
+    path('', MainPageView.as_view(), name="home_page"),
     path('search', SearchPageView.as_view(), name="search_results"),
 ]
