a35a0adc6e33cbb3c4984e5b18292b8b211fd43e
Ofek Buchnik <ofekyvb@gmail.com>
2023-03-25 17:08:58 +0300

    relations ml model, linkedin scraper that collects data into a data.xlsx file under the scrapers folder inside the backend

--- backend/processing/preprocessing.py
+++ backend/processing/preprocessing.py
@@ -4,6 +4,7 @@ import nltk
 from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
 from word2number import w2n
+from googletrans import Translator
 
 nltk.load('stopwords')
 nltk.load('wordnet')
@@ -12,11 +13,10 @@ nltk.load('wordnet')
 def preprocess_job(job_description: str) -> str:
     job_description = job_description.lower().strip()
 
-    # Remove HTML tags and URLs
-    job_description = re.sub('<[^<]+?>', '', job_description)
-    job_description = re.sub(r'http\S+', '', job_description)
-    job_description = re.sub(r'[^ \nA-Za-z0-9À-ÖØ-öø-ÿ/]+', '', job_description)
-    job_description = re.sub(r'[\\/×\^\]\[÷]', ' ', job_description)
+    if contains_hebrew(job_description):
+        job_description = translate_to_english(job_description)
+
+    job_description = remove_unwanted_characters(job_description)
 
     # Tokenize the text into individual words
     words = nltk.word_tokenize(job_description)
@@ -27,6 +27,29 @@ def preprocess_job(job_description: str) -> str:
     filtered_tokens = [w for w in words if w not in stop_words and pattern.match(w)]
 
     # Replace number words with numeric equivalents
+    filtered_tokens = numbers_to_words(filtered_tokens)
+
+    # Apply lemmatization to the words
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(word) for word in filtered_tokens]
+
+    return " ".join(lemmatized_tokens)
+
+
+def translate_to_english(hebrew_text, source_language='he', target_language='en') -> str:
+    """Translates hebrew text to english text"""
+
+    translator = Translator(service_urls=['translate.google.com'])
+    translation = translator.translate(hebrew_text, src=source_language, dest=target_language)
+    return translation.text
+
+
+def numbers_to_words(filtered_tokens: list) -> list:
+    """
+    Receives list of words and numbers, converts the numbers to english words.
+    Ex: '5' to 'five'.
+    """
+
     for i in range(len(filtered_tokens)):
         token = filtered_tokens[i]
 
@@ -39,8 +62,25 @@ def preprocess_job(job_description: str) -> str:
             except ValueError:
                 continue
 
-    # Apply lemmatization to the words
-    lemmatizer = WordNetLemmatizer()
-    lemmatized_tokens = [lemmatizer.lemmatize(word) for word in filtered_tokens]
+    return filtered_tokens
 
-    return " ".join(lemmatized_tokens)
+
+def contains_hebrew(text):
+    hebrew_regex = re.compile(r'[\u0590-\u05FF]')
+    return bool(hebrew_regex.search(text))
+
+
+def remove_unwanted_characters(text: str) -> str:
+    """
+    Regex pattern breakdown:
+    1. Remove HTML tags
+    2. Remove URLs starting with 'http'
+    3. Remove any character that is not a space, newline, alphanumeric, or specific accented characters
+    4. Remove specific special characters: '/', '\', '×', '^', ']', '[', '÷'
+    """
+
+    combined_pattern = r'<[^<]+?>' + '|' + \
+                       r'http\S+' + '|' + \
+                       r'[^ \nA-Za-z0-9À-ÖØ-öø-ÿ/]+' + '|' + \
+                       r'[\\/×\^\]\[÷]'
+    return re.sub(combined_pattern, ' ', text)
