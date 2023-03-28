1d37a1bbf334a101bff4de579587928429869e2b
Ofek Buchnik <ofekyvb@gmail.com>
2023-03-28 18:59:06 +0300

    dockerfile fix, github actions hanges.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
-from jobsearch.views import saml_auth
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('jobsearch.urls')),
     path('saml2/', include('djangosaml2.urls')),
-    path('api/saml-auth/', saml_auth),
     re_path(r'^.*', TemplateView.as_view(template_name="index.html")),
 ]
